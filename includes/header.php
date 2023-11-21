59f2e94fe395b71baa2dccb0bb9f867dfb8d6aec
Pvngu <omargt151102@gmail.com>
2023-11-21 03:05:00 -0800

    little updates for code readibility

--- includes/header.php
+++ includes/header.php
@@ -4,14 +4,14 @@
             <h1 class="bizName">MGR MAQUINADOS</h1>
         </div>
         <div class="rightContent">
-                <div class="userProfile">
-                    <div class="userText">
-                        <div class="name"><?= htmlspecialchars($user["nombre_usuario"]) ?></div>
-                        <div class="job"><?= htmlspecialchars($user["tipo_cuenta"]) ?></div>
-                    </div>
-                    <div class="profilePicture">
-                        <i class='bx bx-user'></i>
-                    </div>
+            <div class="userProfile">
+                <div class="userText">
+                    <div class="name"><?= htmlspecialchars($user["nombre_usuario"]) ?></div>
+                    <div class="job"><?= htmlspecialchars($user["tipo_cuenta"]) ?></div>
+                </div>
+                <div class="profilePicture">
+                    <i class='bx bx-user'></i>
+                </div>
             </div>
         </div>
     </div>
--- includes/sidebar_views.php
+++ includes/sidebar_views.php
@@ -1,28 +1,28 @@
 <!-- settings menu -->
 <dialog class="settingsModal modal">
-            <div class="modalHeader">
-                <h1>Configuracion</h1>
-                <i class='bx bx-x closeBtnModalS modalX'></i>
-            </div>
-            <div class="modalContent stgModal">
-                <div class="switchContainer">
-                    <h2>Tema</h2>
-                    <div class="theme-switcher">
-                        <input type="radio" id="light-theme" name="themes">
-                        <label for="light-theme">
-                            <span>
-                                <i class='bx bxs-sun'></i> Brillo
-                            </span>
-                        </label>
-                        <input type="radio" id="dark-theme" name="themes">
-                        <label for="dark-theme">
-                            <span>
-                                <i class='bx bxs-moon'></i> Dark
-                            </span>
-                        </label>
-                        <span class="slider"></span>
-                    </div>
+    <div class="modalHeader">
+        <h1>Configuracion</h1>
+        <i class='bx bx-x closeBtnModalS modalX'></i>
+    </div>
+        <div class="modalContent stgModal">
+            <div class="switchContainer">
+                <h2>Tema</h2>
+                <div class="theme-switcher">
+                    <input type="radio" id="light-theme" name="themes">
+                    <label for="light-theme">
+                        <span>
+                        <i class='bx bxs-sun'></i> Brillo
+                        </span>
+                    </label>
+                    <input type="radio" id="dark-theme" name="themes">
+                    <label for="dark-theme">
+                        <span>
+                            <i class='bx bxs-moon'></i> Dark
+                        </span>
+                    </label>
+                    <span class="slider"></span>
                 </div>
+            </div>
                 <ul>
                     <li>
                         <a class="openModalAS">
@@ -38,68 +38,67 @@
                     </li>
                 </ul>
             </div>
-        </dialog>
-        <!-- Account settings -->
-        <dialog class="accountStgModal modal">
-            <div class="modalHeader">
-                <h1>Configuracion de cuenta</h1>
-                <i class='bx bx-x closeBtnModalAS modalX'></i>
-            </div>
-            <div class="modalContent accStgModal">
-                <ul>
-                    <h2>Informacion de cuenta</h2>
-                    <li>
-                        <i class='bx bx-hash'></i>
-                        <span>ID del usuario: <?= $user["empleado_id"] ?></span>
-                    </li>
-                    <li>
-                        <i class='bx bx-user'></i>
-                        <span>Nombre: <?= $user["nombre"] ?></span>
-                    </li>
-                    <li>
-                        <i class='bx bx-user'></i>
-                        <span>Nombre de usuario: <?= $user["nombre_usuario"] ?></span>
-                    </li>
-                    <li>
-                        <i class='bx bx-user'></i>
-                        <span>Tipo de cuenta: <?= $user["tipo_cuenta"] ?></span>
-                    </li>
-                </ul>
-            </div>
-        </dialog>
-        <!-- About us -->
-        <dialog class="aboutUsModal modal">
-                <div class="modalHeader">
-                    <h1>Acerca de la pagina</h1>
-                    <i class='bx bx-x closeBtnModalAU modalX'></i>
-                </div>
-                <div class="modalContent AUModal">
-                    <p>En este sistema, nos dedicamos a proporcionar herramientas avanzadas y fáciles de usar para ayudarte a mantener un control preciso de tus existencias. Optimiza tus procesos, reduce costos y mejora la eficiencia con nuestras funciones intuitivas.</p>
-                    <h2>Mision</h2>
-                    <p>Facilitar la gestión de inventarios para empresas de todos los tamaños, permitiéndote tomar decisiones informadas y maximizar la eficiencia operativa.</p>
-                    <h2>Caracteristicas Destacadas</h2>
-                    <ul>
-                        <li>Seguimiento en tiempo real</li>
-                        <li>Alertas de reabastecimiento</li>
-                        <li>Informes detallados</li>
-                        <li>Integracion con sistemas de punto de venta (POS)</li>
-                    </ul>
-                    <p></p>
-                </div>
-        </dialog>
-        <!-- Logout menu -->
+</dialog>
+<!-- Account settings -->
+<dialog class="accountStgModal modal">
+    <div class="modalHeader">
+        <h1>Configuracion de cuenta</h1>
+        <i class='bx bx-x closeBtnModalAS modalX'></i>
+    </div>
+    <div class="modalContent accStgModal">
+        <ul>
+            <h2>Informacion de cuenta</h2>
+            <li>
+                <i class='bx bx-hash'></i>
+                <span>ID del usuario: <?= $user["empleado_id"] ?></span>
+            </li>
+            <li>
+                <i class='bx bx-user'></i>
+                <span>Nombre: <?= $user["nombre"] ?></span>
+            </li>
+            <li>
+                <i class='bx bx-user'></i>
+                <span>Nombre de usuario: <?= $user["nombre_usuario"] ?></span>
+            </li>
+            <li>
+                <i class='bx bx-user'></i>
+                <span>Tipo de cuenta: <?= $user["tipo_cuenta"] ?></span>
+            </li>
+        </ul>
+    </div>
+</dialog>
+<!-- About us -->
+<dialog class="aboutUsModal modal">
+    <div class="modalHeader">
+        <h1>Acerca de la pagina</h1>
+        <i class='bx bx-x closeBtnModalAU modalX'></i>
+    </div>
+    <div class="modalContent AUModal">
+        <p>En este sistema, nos dedicamos a proporcionar herramientas avanzadas y fáciles de usar para ayudarte a mantener un control preciso de tus existencias. Optimiza tus procesos, reduce costos y mejora la eficiencia con nuestras funciones intuitivas.</p>
+        <h2>Mision</h2>
+        <p>Facilitar la gestión de inventarios para empresas de todos los tamaños, permitiéndote tomar decisiones informadas y maximizar la eficiencia operativa.</p>
+        <h2>Caracteristicas Destacadas</h2>
+        <ul>
+            <li>Seguimiento en tiempo real</li>
+            <li>Alertas de reabastecimiento</li>
+            <li>Informes detallados</li>
+            <li>Integracion con sistemas de punto de venta (POS)</li>
+        </ul>
+    </div>
+</dialog>
+<!-- Logout menu -->
 <dialog class="logoutModal modal">
-            <div class="modalHeader">
-                <h1>Cerrar sesion</h1>
-                <i class='bx bx-x closeBtnModalL modalX'></i>
+    <div class="modalHeader">
+        <h1>Cerrar sesion</h1>
+        <i class='bx bx-x closeBtnModalL modalX'></i>
+    </div>
+    <div class="modalContent lgtModal">
+        <div class="logoutText">Esta seguro que quiere cerrar la sesion?</div>
+            <div class="logoutButtons">
+                <button id="logoutClose" class = "closeBtnModalL">Cancelar</button>
+                <form action="logout.php" method="post">
+                    <button id="logoutOk" type="submit">Ok</button>
+                </form>
             </div>
-            <div class="modalContent lgtModal">
-                <div class="logoutText">Esta seguro que quiere cerrar la sesion?</div>
-                <div class="logoutButtons">
-                    <button id="logoutClose" class = "closeBtnModalL">Cancelar</button>
-                    <form action="logout.php" method="post">
-                        <button id="logoutOk" type="submit">Ok</button>
-                    </form>
-                </div>
-            </div>
-        </dialog>
+        </div>
+</dialog>
